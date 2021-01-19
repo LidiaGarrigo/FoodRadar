61372ddb8a6db919d66f54d61f02fa19d6bce35d
= <=>
2021-01-19 23:52:47 +0100

    register

--- README.md
+++ README.md
@@ -28,8 +28,8 @@ Hemos buscado un tema que motive a ambos y dónde podamos aplicar lo aprendido e
 
 La aplicación se va a llamar FoodRadar. Consitirá en un app que te permitirá:
 
-Tener un listado actualizado de recetas con posibilidad de filtrarlas por nombre y categorias (desayuno, cena etc).
-Disponer de un escáner de codigos de productos que te relacionará con las recetas.
+* Tener un listado actualizado de recetas con posibilidad de filtrarlas por nombre y categorias (desayuno, cena etc).
+* Disponer de un escáner de codigos de productos que te relacionará con las recetas.
 Status:
 
 Hemos creado un kanban organzinado las tareas a realizar y planteado los wireframes de la app. También hemos comprobado la accessibilidad de las apis (datos) y creado una primera aproximación al escaner de barras.
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,7 @@
+/* import { AngularFireAuth } from '@angular/fire/auth'; */
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+  password = new FormControl('', [Validators.required]);
+
+  constructor(private fb: FormBuilder,/*  private auth: AngularFireAuth */ private router: Router) {
+
+  }
 
   ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z]).{7,}')]],
+      confirmPassword: ['', [Validators.required,
+                            Validators.pattern('(?=.*[a-z])(?=.*[A-Z]).{7,}')]],
+    });
+  }
+  createUser(){
+/*     const{email, password} = this.registerForm.value;
+    this.auth.createUserWithEmailAndPassword(email, password).then(user => {
+      console.log('RegisterComponent => createUser => user', user);
+      this.router.navigate(['/task-list']);
+    }) */
+    console.log(this.registerForm.value);
+
   }
 
 }
