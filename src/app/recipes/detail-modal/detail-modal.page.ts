2db82a0b32d47f8e91f71b2cf20a89c507382b7c
= <=>
2021-01-21 11:14:03 +0100

    infoModal

--- src/app/recipes/detail-modal/detail-modal.page.ts
+++ src/app/recipes/detail-modal/detail-modal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { RecipesService } from 'src/app/services/recipes.service';
 
 @Component({
   selector: 'app-detail-modal',
@@ -7,10 +8,12 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./detail-modal.page.scss'],
 })
 export class DetailModalPage implements OnInit {
-
-  constructor(private modalController: ModalController,) { }
+  public recipes = [];
+  constructor(private modalController: ModalController,private RecipeService: RecipesService,) { }
 
   ngOnInit() {
+    this.RecipeService.getRecipes$()
+      .subscribe((data)=> this.recipes = data.hits);
   }
   async closeModal(){
     await this.modalController.dismiss({
--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -7,15 +7,16 @@ import { Observable } from 'rxjs';
 })
 export class RecipesService {
 
-  API_URL = 'https://api.edamam.com/search?app_id=7eae9a21&app_key=86cd235824988a5b3411ccff412225a6&q=q';
+  API_URL = 'https://api.edamam.com/search?app_id=7eae9a21&app_key=86cd235824988a5b3411ccff412225a6&q=recipe';
   API_ID = '7eae9a21';
   API_KEY = '86cd235824988a5b3411ccff412225a6';
-  API_FOOD;
-
 
 constructor(private http: HttpClient) { }
 
 getRecipes$(): Observable<any>{
+  return this.http.get(this.API_URL);
+}
+getRecipe$(): Observable<any>{
   return this.http.get(this.API_URL);
 }
 }
